2e6828150bd5214a1bab8907d8120c3353b78438
alexross2502 <alexross19941994@gmail.com>
2022-12-30 10:19:48 +0200

    test: test2

--- index.js
+++ index.js
@@ -10,20 +10,26 @@ const errorHandler = require("./middleware/ErrorHandlingMiddleware");
 app.use(cors({
   origin: "*"
 }))
-/*
+let permitted = (origin) => {
+if(
+  origin == 'https://test-app22334455.herokuapp.com' && origin == 'https://test-app22334455.herokuapp.com/#' && origin == 'https://test-app22334455.herokuapp.com/clients' && origin == 'https://test-app22334455.herokuapp.com/masters' && origin == 'https://test-app22334455.herokuapp.com/admins' && origin == 'https://test-app22334455.herokuapp.com/towns' && origin == 'https://test-app22334455.herokuapp.com/reservation'
+) return true 
+else return false
+}
+
 app.get("*", function (req, res, next) {
-  if(req.rawHeaders[5] == 'https://test-app22334455.herokuapp.com') {next()} else res.json({ msg: "Конфиденциальные данные" });
+  if(permitted(req.rawHeaders[5]) == true) {next()} else res.json({ msg: "Конфиденциальные данные" });
 });
 
 app.post("*", function (req, res, next) {
-  if(req.rawHeaders[5] == 'https://test-app22334455.herokuapp.com') {next()} else res.json({ msg: "Конфиденциальные данные" });
+  if(permitted(req.rawHeaders[5]) == true) {next()} else res.json({ msg: "Конфиденциальные данные" });
 });
 
 app.delete("*", function (req, res, next) {
-  if(req.rawHeaders[5] == 'https://test-app22334455.herokuapp.com') {next()} else res.json({ msg: "Конфиденциальные данные" });
+  if(permitted(req.rawHeaders[5]) == true) {next()} else res.json({ msg: "Конфиденциальные данные" });
 });
 
-*/
+
 app.use(express.json());
 app.use("/api", router);
 //обработка ошибок, последний middleware
